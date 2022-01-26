70845d926cb6ba51dd5fa1ee8bb159fb6d2929f9
SYBIOTE <54761979+SYBIOTE@users.noreply.github.com>
2022-01-26 15:41:40 +0530

    Revert "added functions to device type"
    This reverts commit 8f2f3ec1e001e4434ec4da50afc43f0bb06f4fe3.

--- plugin/src/main/java/org/godotengine/plugin/vr/oculus/platform/api/OvrSystem.kt
+++ plugin/src/main/java/org/godotengine/plugin/vr/oculus/platform/api/OvrSystem.kt
@@ -5,48 +5,6 @@ package org.godotengine.plugin.vr.oculus.platform.api
 import org.godotengine.plugin.vr.oculus.platform.OvrPlatformPlugin
 
 /**
- * Oculus device type.
- */
-enum class DeviceType(internal val rangeStart: Int, internal val rangeEnd: Int = rangeStart) {
-    /**
-     * Unknwon device type.
-     */
-    UNKNOWN(-1),
-    
-    /**
-     * Device type for the Oculus Quest 1.
-     */
-    OCULUS_QUEST(256, 319),
-
-    /**
-     * Device type for the Oculus Quest 2.
-     */
-    OCULUS_QUEST2(320, 383);
-
-    companion object {
-        fun toDeviceType(value: Int): DeviceType {
-            for(deviceType in values()) {
-                if (deviceType.rangeStart <= value && value <= deviceType.rangeEnd) {
-                    return deviceType;
-                }
-            }
-            return UNKNOWN
-        }
-    }
-}
-
-/**
- * Return the current [DeviceType] type.
- */
-fun OvrMobilePlugin.getDeviceType(): DeviceType {
-    return if (isSharedLibLoaded()) {
-        DeviceType.toDeviceType(nativeGetDeviceType())
-    } else {
-        DeviceType.UNKNOWN
-    }
-}
-
-/**
  * Return the current driver version.
  */
 fun OvrPlatformPlugin.getDriverVersion(): String {
@@ -56,6 +14,5 @@ fun OvrPlatformPlugin.getDriverVersion(): String {
         ""
     }
 }
-private external fun nativeGetDeviceType(): Int
 
 private external fun nativeGetDriverVersion(): String
